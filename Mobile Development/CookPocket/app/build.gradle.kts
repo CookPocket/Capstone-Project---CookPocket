38fa4e14e34eda0cc11ee0811eee36c028b0afa3
Muhammad Zaqly Luluang <muhammadzaqly01@gmail.com>
2024-11-30 15:15:17 +0800

    tambahan sodara

--- Mobile Development/CookPocket/app/build.gradle.kts
+++ Mobile Development/CookPocket/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.capstone.cookpocket"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.capstone.cookpocket"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- Mobile Development/CookPocket/app/src/main/java/com/capstone/cookpocket/view/ui/adapter/AdapterActivity.kt
+++ Mobile Development/CookPocket/app/src/main/java/com/capstone/cookpocket/view/ui/adapter/AdapterActivity.kt
@@ -1,16 +1,17 @@
-package com.capstone.cookpocket
+package com.capstone.cookpocket.view.ui.adapter
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.capstone.cookpocket.R
 
-class MainActivity : AppCompatActivity() {
+class AdapterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_adapter)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

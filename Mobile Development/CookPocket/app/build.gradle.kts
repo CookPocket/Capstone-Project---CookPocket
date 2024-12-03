fbd4b442ac822595b4a28e0321a341cc06b32106
Iccank Ar <iccankar04@gmail.com>
2024-12-03 18:56:32 +0800

    Perubahan gradle

--- Mobile Development/CookPocket/app/build.gradle.kts
+++ Mobile Development/CookPocket/app/build.gradle.kts
@@ -57,21 +57,21 @@ dependencies {
     implementation(libs.material.v190)
 
     // Retrofit dan Gson converter untuk API request dan response
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     // Kotlin Coroutines untuk asynchronous programming
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
     // Lifecycle components untuk ViewModel dan LiveData
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     // DataStore Preferences untuk penyimpanan data yang bersifat key-value
     implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     // Glide untuk pengelolaan gambar
-    implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }

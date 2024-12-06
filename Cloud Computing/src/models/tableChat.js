d7f23a9d428ab46d1c5cab7facb53750fe64be98
NicoRamdani <nicoramdani3@gmail.com>
2024-12-06 22:40:09 +0700

    update file

--- Cloud Computing/src/models/tableChat.js
+++ Cloud Computing/src/models/tableChat.js
@@ -1,38 +1,28 @@
-const configDb = require('../config/database');
-
-const getMessage = async (senderId, receiver_Id) => {
-    const [data] = await configDb.execute(
-        `SELECT c.*,
-        sender.name AS sender_name,
-        receiver.name AS receiver_name
-        FROM chat c
-        JOIN user sender ON c.sender_id =sender id_user
-        WHERE (c.sender_id = ? AND c.receiver_id= ?)
-        OR (c.sender_id = ? AND c.receiver_id= ?)
-        ORDER BY c.created_at ASC`,
-        [senderId, receiver_Id, receiver_Id, senderId]
-    );
-    return data;
-}
-
+const db = require('../config/database');
 
 const createMessage = async (senderId, receiverId, message) => {
-    const [resultQuery] = await configDb.execute(
-        `INSERT INTO chat(sender_id, receiver_id, message, status, created_at)
-        VALUES(?, ?, ?, 'sent', NOW())`,
-        [senderId, receiverId, message]
-    );
-    return resultQuery.insertId
+    const query = `
+        INSERT INTO chat (sender_id, receiver_id, message) 
+        VALUES (?, ?, ?)`;
+        
+    const [result] = await db.execute(query, [senderId, receiverId, message]);
+    return result.insertId;
+};
+
+
+const getChatHistory = async (senderId, receiverId) => {
+    const query = `
+        SELECT * FROM chat
+        WHERE (sender_id = ? AND receiver_id = ?)
+        OR (sender_id = ? AND receiver_id = ?)
+        ORDER BY created_at ASC
+    `;
+    const [data] = await db.execute(query, [senderId, receiverId, receiverId, senderId]);
+    return data;
+};
+
+module.exports = {
+    createMessage,
+    getChatHistory
+
 }
-
-
-const updateMessage = async (idChat, status) => {
-    const [resultQuery] = await configDb.execute(
-        `UPDATE chat SET status= ? WHERE id_chat= ?`,
-        [status, idChat]
-    );
-    return resultQuery.affectedRows;
-}
-
-
-module.exports = { getMessage, createMessage, updateMessage };
--- Cloud Computing/src/server.js
+++ Cloud Computing/src/server.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const htpp = require('http')
 require('dotenv').config();
+
 const PORT = process.env.PORT || 4000;
-const routesHomePage = require('./routes/home.js');
+const initSocket = require('./socket');
+const routesChat = require('./routes/chat.js');
+
 const routesUsers = require('./routes/users');
 const logsRequest = require('./middleware/logs.js');
 
 const app = express();
+const server = htpp.createServer(app)
 
-app.use(express.json({ type: 'application/json' }));
+app.use(express.json());
 app.use(logsRequest);
 
-app.use('/v1', routesUsers);
-app.use('/', routesHomePage);
+//path login/register
+app.use('/api', routesUsers);
 
-app.listen(PORT, () => {
+//path fitur chat
+app.use('/api/chat', routesChat);
+
+initSocket(server);
+
+server.listen(PORT, () => {
     console.log(`Server berjalan pada http://localhost:${PORT}`);
 });

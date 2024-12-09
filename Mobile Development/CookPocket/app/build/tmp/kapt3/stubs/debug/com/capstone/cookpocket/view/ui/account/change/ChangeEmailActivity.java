ff60b10dd5a61f1b1288216172343fc580235434
Muhammad Zaqly Luluang <muhammadzaqly01@gmail.com>
2024-12-09 17:15:10 +0800

    penambahan add product dan modif search

---
+++ Mobile Development/CookPocket/app/build/tmp/kapt3/stubs/debug/com/capstone/cookpocket/view/ui/account/change/ChangeEmailActivity.java
@@ -0,0 +1,29 @@
+package com.capstone.cookpocket.view.ui.account.change;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000F\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u000f\u001a\u00020\u00102\u0006\u0010\u0011\u001a\u00020\u0012H\u0002J\b\u0010\u0013\u001a\u00020\u0010H\u0002J\u0012\u0010\u0014\u001a\u00020\u00102\b\u0010\u0015\u001a\u0004\u0018\u00010\u0016H\u0014J\u0010\u0010\u0017\u001a\u00020\u00102\u0006\u0010\u0011\u001a\u00020\u0012H\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\t\u001a\u00020\nX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000b\u001a\u00020\fX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\r\u001a\u00020\u000eX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0018"}, d2 = {"Lcom/capstone/cookpocket/view/ui/account/change/ChangeEmailActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "btnSimpanEmail", "Landroid/widget/Button;", "changeEmailEditText", "Lcom/google/android/material/textfield/TextInputEditText;", "edChangeEmail", "Lcom/google/android/material/textfield/TextInputLayout;", "ivBackEmail", "Landroid/view/View;", "progressBarEmail", "Landroid/widget/ProgressBar;", "tvCurrentEmail", "Landroid/widget/TextView;", "changeEmail", "", "newEmail", "", "loadCurrentEmail", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "showChangeEmailConfirmationDialog", "app_debug"})
+public final class ChangeEmailActivity extends androidx.appcompat.app.AppCompatActivity {
+    private com.google.android.material.textfield.TextInputLayout edChangeEmail;
+    private android.widget.Button btnSimpanEmail;
+    private android.widget.ProgressBar progressBarEmail;
+    private com.google.android.material.textfield.TextInputEditText changeEmailEditText;
+    private android.view.View ivBackEmail;
+    private android.widget.TextView tvCurrentEmail;
+    
+    public ChangeEmailActivity() {
+        super();
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void loadCurrentEmail() {
+    }
+    
+    private final void showChangeEmailConfirmationDialog(java.lang.String newEmail) {
+    }
+    
+    private final void changeEmail(java.lang.String newEmail) {
+    }
+}
